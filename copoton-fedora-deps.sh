2fdce76bcf8b58dc0ecc0d0ccee1e97fb09ab612
paul <paul@iosyg.com>
2017-04-08 13:13:38 +0100

    more examples

---
+++ copoton-fedora-deps.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+dnf install libX11-devel libXcomposite-devel libXdamage-devel libXfixes-devel libXext-devel libXrender-devel libXrandr-devel libXinerama-devel pkgconfig make xorg-x11-proto-devel xprop xwininfo pcre2-devel libconfig-devel libdrm-devel libGL-devel dbus-c++-devel dbus-python-devel asciidoc
